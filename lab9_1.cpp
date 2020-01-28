b43c16735af6faa35aaca48530374263e922f9e0
lab302-37 <lab302-37@labeng.local>
2020-01-28 16:23:48 +0700

    v1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,26 +3,37 @@ using namespace std;
 
 int main(){
 	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	string grade;
 	cout << "Please input grade of each student (A-F) or input 0 to exit.";
 	do{
-		cout << "Student [" << "]:";
+		cout << "Student [" << count[5] +1 << "]:";
 		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
+		if( grade == "A" ) {
+			count[0] ++;
+			count[5]++;
+		}else if( grade == "B" ) { 
+			count[1]++;
+			count[5]++;
+		}else if( grade == "C" ) {
+			count[2]++;
+			count[5]++;
+		}else if( grade == "D" ) {
+			count[3]++;
+			count[5]++;
+		}else if( grade == "E" ) {
+			count[4]++;
+			count[5]++;
+		}else{
+			cout << "Wrong input. Please input again.";
 		} 
-	}while(true);
+	}while( grade != "0");
 	
 	
-	cout << "In total ? students.";
+	cout << "In total" << count[5] << "students.";
 	cout << "A = " << count[0] <<",";
 	cout << "B = " << count[1] <<",";	
-	//	and so on ... for grade = C, D, F	
-	
+	cout << "C = " << count[2] <<",";
+	cout << "D = " << count[3] <<",";
+	cout << "F = " << count[4] <<",";	
+		return 0;
 	}
-	return 0;
-}
