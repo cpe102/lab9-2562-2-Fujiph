08c96f4f8dc0a3f2c4e00920022b84afb065f44d
lab302-37 <lab302-37@labeng.local>
2020-01-28 16:34:16 +0700

    v2

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -6,21 +6,19 @@ using namespace std;
 
 int main() {
     ifstream source;
-    float sum ;
+    float sum ,y ;
     int count = 0;
     source.open ("score.txt");
     string textline;
     while(getline(source,textline)){
         sum += atof(textline.c_str());
+        y += pow(atof(textline.c_str()),2);
         count++;
     }
     float mean = sum/count;
-    float SD = sqrt(pow(sum - mean,2)/count);
+    float SD = sqrt(y/count-pow(mean,2));
     cout << "Number of data = " << count << endl ;
     cout << "Mean = " << mean <<endl ;
     cout << "Standard deviation = " << SD << endl;
     source.close();
 }
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
